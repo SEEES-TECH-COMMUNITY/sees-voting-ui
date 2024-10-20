185235f245e82c44501aba81e0ff9e764b4276d7
Udochukwuka Onyela <udochukwukaonyela@gmail.com>
2024-10-20 23:30:19 +0100

    chore: updated style guide

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,8 +9,35 @@ const config: Config = {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        grey: {
+          800: "#AAA",
+          700: "#A7B5C1",
+          650: "#6D6D6D",
+          600: "#F3F3F3",
+          400: "#FBFBFB",
+        },
+        dark: {
+          900: "#000F1E",
+        },
+        blue: {
+          600: "#1573FF",
+          200: "#D8E7FF",
+          placeholder: {
+            600: "#002445",
+          },
+        },
+        red: {
+          500: "#FF1515",
+        },
+        green: {
+          800: "#249607",
+          500: "#00B298",
+          300: "#D5FFCB",
+        },
+        yellow: {
+          800: "#F2A102",
+          300: "#FFFACB",
+        },
       },
     },
   },
