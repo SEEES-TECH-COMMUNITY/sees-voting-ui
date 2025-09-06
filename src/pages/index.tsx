fed2445339eb4be7c6ede70f5482f7a566b38421
Onyela Udochukwuka <udochukwukaonyela@gmail.com>
2025-09-06 12:50:47 +0100

    refactor: update Home component styling for improved readability and consistency

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,12 @@
 export default function Home() {
-
-    return (
-        <main className="flex min-h-screen flex-col bg-primary-375/10 p-6 space-y-4">
-            <h1 className="text-5xl font-extrabold tracking-tight text-secondary sm:text-[5rem] text-center py-2">
-                SEEES <span className="text-primary">ELECTION</span> 2023/2024
-            </h1>
-            <p className="text-center font-medium w-full">Voting has concluded the results would be announced soon.</p>
-        </main>
-    );
+  return (
+    <main className="flex min-h-screen flex-col p-6 space-y-4 text-black">
+      <h1 className="text-5xl font-extrabold tracking-tight text-dark-900 sm:text-[5rem] text-center py-2">
+        SEEES <span className="text-primary">ELECTION</span> 2023/2024
+      </h1>
+      <p className="text-center font-medium w-full">
+        Voting has concluded the results would be announced soon.
+      </p>
+    </main>
+  );
 }
