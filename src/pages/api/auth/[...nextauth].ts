26f05cb562023091f2d469b8dbd6576aca47ab59
Onyela Udochukwuka <udochukwukaonyela@gmail.com>
2025-09-07 21:59:16 +0100

    refactor: remove GoogleProvider and clean up authorization logic in NextAuth configuration

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,6 +1,5 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import NextAuth, { NextAuthOptions } from "next-auth";
-import GoogleProvider from "next-auth/providers/google";
 import CredentialsProvider from "next-auth/providers/credentials";
 // eslint-disable-next-line @typescript-eslint/no-require-imports
 require("dotenv").config();
@@ -12,15 +11,6 @@ type NextAuthOptionsCallback = (
 const nextAuthOptions: NextAuthOptionsCallback = (req, res) => {
   return {
     providers: [
-      GoogleProvider({
-        clientId: String(process.env.GOOGLE_CLIENT_ID),
-        clientSecret: String(process.env.GOOGLE_CLIENT_SECRET),
-        authorization: {
-          params: {
-            max_age: parseInt(process.env.NEXT_PUBLIC_STORAGE_TIME || ""),
-          },
-        },
-      }),
       CredentialsProvider({
         id: "auth-credentials",
         credentials: {
@@ -28,24 +18,26 @@ const nextAuthOptions: NextAuthOptionsCallback = (req, res) => {
           password: { label: "Password", type: "password" },
           fingerprint: { label: "Fingerprint", type: "text" },
         },
-        async authorize(credentials, req) {
-          console.log({ credentials });
-          // Add logic here to look up the user from the credentials supplied
-          const user = {
-            id: "1",
-            name: "J Smith",
-            email: "jsmith@example.com",
-          };
-
-          if (user) {
-            // Any object returned will be saved in `user` property of the JWT
-            return user;
-          } else {
-            // If you return null then an error will be displayed advising the user to check their details.
+        async authorize(credentials) {
+          const response = await fetch(
+            `${process.env.NEXT_PUBLIC_API_URL}/auth/login`,
+            {
+              method: "POST",
+              body: JSON.stringify({
+                mat_number: credentials?.["mat-number"],
+                password: credentials?.password,
+                finger_print: credentials?.fingerprint,
+              }),
+              headers: { "Content-Type": "application/json" },
+            }
+          );
+          const responseJson = await response.json();
+          if (!responseJson?.success) {
             return null;
-
-            // You can also Reject this callback with an Error thus the user will be sent to the error page with the error message as a query parameter
           }
+          const cookies = response.headers.getSetCookie();
+          res.setHeader("Set-Cookie", cookies);
+          return responseJson.success;
         },
       }),
     ],
@@ -53,6 +45,21 @@ const nextAuthOptions: NextAuthOptionsCallback = (req, res) => {
     pages: {
       signIn: "/login",
       error: "/login",
+    },
+    callbacks: {
+      async session() {
+        const headers = new Headers();
+        headers.set("Cookie", req.headers?.cookie || "");
+        headers.set("Content-Type", "application/json");
+        const response = await fetch(
+          `${process.env.NEXT_PUBLIC_API_URL}/students/me`,
+          {
+            headers,
+          }
+        );
+        const responseJson = await response.json();
+        return responseJson;
+      },
     },
   };
 };
