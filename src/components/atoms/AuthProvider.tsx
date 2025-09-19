0ad02eae6ea15307a4bf748375c1c597d43f41e2
Onyela Udochukwuka <udochukwukaonyela@gmail.com>
2025-09-19 14:19:39 +0100

    fix: update unprotected routes and improve loading state handling in AuthProvider

--- src/components/atoms/AuthProvider.tsx
+++ src/components/atoms/AuthProvider.tsx
@@ -1,30 +1,29 @@
-
 import { saveToken } from "@src/utils/function/storage";
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/router";
 import * as React from "react";
 
-
-
 const AuthProvider: React.FC = () => {
   const { status, data } = useSession();
   const { pathname, push } = useRouter();
-  const unProtectedRoutes = ["/login", "/admin/result"];
+  const unProtectedRoutes = ["/activate", "/login", "/admin/result"];
 
   React.useEffect(() => {
     if (
       status === "unauthenticated" ||
       (status === "authenticated" && !data.id)
     ) {
-      if (!unProtectedRoutes.includes(pathname)) {
+      if (!unProtectedRoutes.includes(pathname) && pathname === "/") {
         push("/login");
       }
+    } else if (status === "loading") {
+      push("/loading");
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [status, pathname]);
   React.useEffect(() => {
     if (data?.token) {
-      saveToken(data?.token)
+      saveToken(data?.token);
     }
   }, [data]);
   return null;
